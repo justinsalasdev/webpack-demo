bb114d6855d819a6bfed6d4bd4bdf14bfd4d1f3e
justinsalasdev <justinsalasdev@gmail.com>
2021-09-05 20:58:34 +0800

    added asset loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from "lodash";
+import "./style.css";
+import imageSample from "./image.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+//for JSON import, destructuring is not allowed
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myImage = new Image();
+  myImage.src = imageSample;
+
+  element.appendChild(myImage);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
