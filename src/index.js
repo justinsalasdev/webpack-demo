89939aaee6d5aa63f7f9c5cce39ccfd2e39612a4
justinsalasdev <justinsalasdev@gmail.com>
2021-09-05 21:09:50 +0800

    added json parsers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import "./style.css";
 import imageSample from "./image.png";
 import Data from "./data.xml";
 import Notes from "./data.csv";
+
+import toml from "./data.toml";
+import yaml from "./data.yaml";
+import json from "./data.json5";
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
+
 //for JSON import, destructuring is not allowed
 
 function component() {
